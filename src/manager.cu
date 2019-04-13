80ea09c07a89f6417b131a30502992b5ae21171d
Michael Katz <mikekatz04@gmail.com>
2019-04-13 14:07:30 +0200

    main cleanup of manager.cu finished

--- src/manager.cu
+++ src/manager.cu
@@ -38,79 +38,57 @@ GPUPhenomHM::GPUPhenomHM (double *freqs_,
 
     // DECLARE ALL THE  NECESSARY STRUCTS
 
-    PhenomHMStorage *pHM_trans = new PhenomHMStorage;
+    pHM_trans = new PhenomHMStorage;
 
-    IMRPhenomDAmplitudeCoefficients *pAmp_trans = new IMRPhenomDAmplitudeCoefficients;
+    pAmp_trans = new IMRPhenomDAmplitudeCoefficients;
 
-    AmpInsPrefactors *amp_prefactors_trans = new AmpInsPrefactors;
+    amp_prefactors_trans = new AmpInsPrefactors;
 
-    PhenDAmpAndPhasePreComp *pDPreComp_all_trans = new PhenDAmpAndPhasePreComp[num_modes];
+    pDPreComp_all_trans = new PhenDAmpAndPhasePreComp[num_modes];
 
-    HMPhasePreComp *q_all_trans = new HMPhasePreComp[num_modes];
+    q_all_trans = new HMPhasePreComp[num_modes];
 
-    std::complex<double> *factorp_trans = new std::complex<double>[num_modes];
+    factorp_trans = new std::complex<double>[num_modes];
+    factorc_trans = new std::complex<double>[num_modes];
 
-    std::complex<double> *factorc_trans = new std::complex<double>[num_modes];
-
-    this->pHM_trans = pHM_trans;
-    this->pAmp_trans = pAmp_trans;
-    this->pDPreComp_all_trans = pDPreComp_all_trans;
-    this->amp_prefactors_trans = amp_prefactors_trans;
-    this->q_all_trans = q_all_trans;
-    this->factorp_trans = factorp_trans;
-    this->factorc_trans = factorc_trans;
 
   if ((to_gpu == 0) || (to_gpu == 2)){
       printf("cpu\n");
-      std::complex<double> *hptilde = new std::complex<double>[num_modes*f_length];
-      std::complex<double> *hctilde = new std::complex<double>[num_modes*f_length];
-      this->hptilde = hptilde;
-      this->hctilde = hctilde;
+      hptilde = new std::complex<double>[num_modes*f_length];
+      hctilde = new std::complex<double>[num_modes*f_length];
   }
   if ((to_gpu == 1) || (to_gpu == 2)){
 
       printf("was here\n");
 
-      double *freqs_geom = new double[f_length];
-      this->freqs_geom = freqs_geom;
+      freqs_geom = new double[f_length];
 
-      double *d_freqs_geom;
       size_t freqs_size = f_length*sizeof(double);
       cudaMalloc(&d_freqs_geom, freqs_size);
-      this->d_freqs_geom = d_freqs_geom;
 
-      unsigned int *d_l_vals, *d_m_vals;
       size_t mode_array_size = num_modes*sizeof(unsigned int);
       cudaMalloc(&d_l_vals, mode_array_size);
       cudaMalloc(&d_m_vals, mode_array_size);
       cudaMemcpy(d_l_vals, l_vals, mode_array_size, cudaMemcpyHostToDevice);
       cudaMemcpy(d_m_vals, m_vals, mode_array_size, cudaMemcpyHostToDevice);
 
-      cuDoubleComplex *d_hptilde, *d_hctilde;
       size_t h_size = num_modes*f_length*sizeof(cuDoubleComplex);
       cudaMalloc(&d_hptilde, h_size);
       cudaMalloc(&d_hctilde, h_size);
 
 
       // DECLARE ALL THE  NECESSARY STRUCTS
-
-      PhenomHMStorage *d_pHM_trans;
       cudaMalloc(&d_pHM_trans, sizeof(PhenomHMStorage));
 
-      IMRPhenomDAmplitudeCoefficients *d_pAmp_trans;
       cudaMalloc(&d_pAmp_trans, sizeof(IMRPhenomDAmplitudeCoefficients));
 
-      AmpInsPrefactors *d_amp_prefactors_trans;
       cudaMalloc(&d_amp_prefactors_trans, sizeof(AmpInsPrefactors));
 
-      PhenDAmpAndPhasePreComp *d_pDPreComp_all_trans;
       cudaMalloc(&d_pDPreComp_all_trans, num_modes*sizeof(PhenDAmpAndPhasePreComp));
 
-      HMPhasePreComp *d_q_all_trans;
       err = cudaMalloc((void**) &d_q_all_trans, num_modes*sizeof(HMPhasePreComp));
       assert(err == 0);
 
-      cuDoubleComplex *d_factorp_trans, *d_factorc_trans;
       size_t complex_factor_size = num_modes*sizeof(cuDoubleComplex);
       err = cudaMalloc(&d_factorp_trans, complex_factor_size);
       assert(err == 0);
@@ -125,24 +103,10 @@ GPUPhenomHM::GPUPhenomHM (double *freqs_,
                            PI_2 /* -1 shift */,
                            0.0};
 
-      double *d_cShift;
       err = cudaMalloc(&d_cShift, 7*sizeof(double));
       assert(err == 0);
       err = cudaMemcpy(d_cShift, &cShift, 7*sizeof(double), cudaMemcpyHostToDevice);
       assert(err == 0);
-
-      this->d_l_vals = d_l_vals;
-      this->d_m_vals = d_m_vals;
-      this->d_hptilde = d_hptilde;
-      this->d_hctilde = d_hctilde;
-      this->d_pHM_trans = d_pHM_trans;
-      this->d_pAmp_trans = d_pAmp_trans;
-      this->d_pDPreComp_all_trans = d_pDPreComp_all_trans;
-      this->d_amp_prefactors_trans = d_amp_prefactors_trans;
-      this->d_q_all_trans = d_q_all_trans;
-      this->d_factorp_trans = d_factorp_trans;
-      this->d_factorc_trans = d_factorc_trans;
-      this->d_cShift = d_cShift;
   }
 
   NUM_THREADS = 256;
@@ -153,13 +117,13 @@ GPUPhenomHM::GPUPhenomHM (double *freqs_,
 
 
   //double t0_;
-  this->t0 = 0.0;
+  t0 = 0.0;
 
   //double phi0_;
-  this->phi0 = 0.0;
+  phi0 = 0.0;
 
   //double amp0_;
-  this->amp0 = 0.0;
+  amp0 = 0.0;
 }
 
 
@@ -248,19 +212,6 @@ void GPUPhenomHM::gpu_gen_PhenomHM(
      cudaDeviceSynchronize();
      err = cudaGetLastError();
      assert(err == 0);
-    /*int i, j;
-    printf("f_length %d\n\n", f_length);
-    double check;
-    for (i=0; i<num_modes; i++){
-        for (j=0; j<f_length; j++){
-            check = std::real(hptilde[i*f_length + j]);
-            if (j % 100 == 0) printf("%e, %e, %e, %e, %e\n", freqs[j], std::real(hptilde[i*f_length + j]), std::imag(hptilde[i*f_length + j]), std::real(hctilde[i*f_length + j]), std::imag(hctilde[i*f_length + j]));
-        }
-    }
-    //this->hptilde = hptilde;
-    printf("\n\n\n\n\n\n\n");
-     printf("\nhptilde %e\n\n", hptilde[0].real());*/
-
 }
 
 
@@ -318,47 +269,24 @@ void GPUPhenomHM::cpu_gen_PhenomHM(
         &t0,
         &phi0,
         &amp0);
-    assert (retcode == 1); //,PD_EFUNC, "IMRPhenomHMCore failed in IMRPhenomHM.");
-    /*int i, j;
-    printf("f_length %d\n\n", f_length);
-    double check;
-    for (i=0; i<num_modes; i++){
-        for (j=0; j<f_length; j++){
-            check = std::real(hptilde[i*f_length + j]);
-            if (j % 100 == 0) printf("%e, %e, %e, %e, %e\n", freqs[j], std::real(hptilde[i*f_length + j]), std::imag(hptilde[i*f_length + j]), std::real(hctilde[i*f_length + j]), std::imag(hctilde[i*f_length + j]));
-        }
-    }
-    //this->hptilde = hptilde;
-    printf("\n\n\n\n\n\n\n");
-     printf("\nhptilde %e\n\n", hptilde[0].real());*/
-
+    assert (retcode == 1); //,PD_EFUNC, "IMRPhenomHMCore failed in
 }
 
 void GPUPhenomHM::Get_Waveform (std::complex<double>* hptilde_, std::complex<double>* hctilde_) {
-  //hptilde[10] = std::complex<double>(10.0, 9.0);
-  //printf("%e\n", hptilde[0].real());
-  //printf("%d %d\n", length_, f_length);
-if ((this->to_gpu == 0) || (this->to_gpu == 2)){
-     memcpy(hptilde_, hptilde, num_modes*f_length*sizeof(std::complex<double>));
-     memcpy(hctilde_, hctilde, num_modes*f_length*sizeof(std::complex<double>));
-}
-  //array_host_[0] = this->hptilde[0];
-  //printf("%e\n", array_host_[0].real());
+
+assert ((to_gpu == 0) || (to_gpu == 2));
+ memcpy(hptilde_, hptilde, num_modes*f_length*sizeof(std::complex<double>));
+ memcpy(hctilde_, hctilde, num_modes*f_length*sizeof(std::complex<double>));
+
 }
 
 void GPUPhenomHM::gpu_Get_Waveform (std::complex<double>* hptilde_, std::complex<double>* hctilde_) {
-  //hptilde[10] = std::complex<double>(10.0, 9.0);
-  //printf("%e\n", hptilde[0].real());
-  //printf("%d %d\n", length_, f_length);
   assert((to_gpu == 1) || (to_gpu == 2));
     cudaError_t err;
      err = cudaMemcpy(hptilde_, d_hptilde, num_modes*f_length*sizeof(std::complex<double>), cudaMemcpyDeviceToHost);
      assert(err == 0);
      cudaMemcpy(hctilde_, d_hctilde, num_modes*f_length*sizeof(std::complex<double>), cudaMemcpyDeviceToHost);
      assert(err == 0);
-
-  //array_host_[0] = this->hptilde[0];
-  //printf("%e\n", array_host_[0].real());
 }
 
 GPUPhenomHM::~GPUPhenomHM() {

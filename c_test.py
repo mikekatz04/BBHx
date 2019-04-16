17f311445473a7d5b25dc9de086908956899c707
Michael Katz <mikekatz04@gmail.com>
2019-04-16 22:54:50 +0200

    c_test updated

--- c_test.py
+++ c_test.py
@@ -5,6 +5,7 @@ from astropy.cosmology import Planck15 as cosmo
 from scipy import constants as ct
 import time
 import pdb
+import matplotlib.pyplot as plt
 
 def test():
     df = 1e-7
@@ -19,22 +20,26 @@ def test():
      m_vals)
 
     #for _ in range(5):
-    for i in range(3):
-        st = time.perf_counter()
-        phenomHM.gen_PhenomHM(m1,  # solar masses
-                     m2,  # solar masses
-                     chi1z,
-                     chi2z,
-                     distance,
-                     inclination,
-                     phiRef,
-                     deltaF,
-                     f_ref)
-        print('cpu', time.perf_counter() - st)
-        check = phenomHM.Likelihood()
-        print('like', check)
+    st = time.perf_counter()
+    phenomHM.gen_PhenomHM(m1,  # solar masses
+                 m2,  # solar masses
+                 chi1z,
+                 chi2z,
+                 distance,
+                 inclination,
+                 phiRef,
+                 deltaF,
+                 f_ref)
+    print('cpu', time.perf_counter() - st)
+    check = phenomHM.Likelihood()
+    print('inner produce base check', check)
 
     hp, hc = phenomHM.Get_Waveform()
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    for i in range(len(m_vals)):
+        ax1.loglog(freq, np.abs(hp[i]))
+        ax2.semilogx(freq, np.unwrap(np.angle(hp[i])))
+    plt.show()
 
 
 
